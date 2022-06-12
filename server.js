b5ef6524f865b04c5f5b4e61618352f30dffa9aa
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-06-12 02:53:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const mongoConfig = require('./config/mongoConfig')
+const contactsRouter = require('./Routes/contactsRouter');
+
+app.use(express.json());
+
+app.use('/contacts', contactsRouter)
+
+app.get('/', (req,res) => {
+    res.status(200).json({msg:'Welcome'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
+})
